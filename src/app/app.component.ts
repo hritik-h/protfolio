cf055ac0cf0c95dea2251f769561b15fdb7f1b5d
hritik.bnp <hritik.bnp@gmail.com>
2024-11-03 21:37:18 +0530

    content window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BoodyComponent } from './boody/boody.component';
+import { BodyComponent } from './body/body.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,BoodyComponent],
+  imports: [RouterOutlet,NavbarComponent,BoodyComponent, BodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
